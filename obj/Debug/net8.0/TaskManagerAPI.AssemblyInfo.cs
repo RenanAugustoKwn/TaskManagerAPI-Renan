8bc583df6179668c862c8261d1d152c011686574
RenanAugustoKwn <renanaugustokwn@outlook.com>
2025-07-12 15:44:12 -0300

    cofig

--- obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae24a2f7834060d834bd2e3ebee88e463fcfb0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1abaf6522f88b9dd665d9412927c5134cd740671")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
